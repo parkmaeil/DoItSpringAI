fbc0b639c0425399b1ca131f5a10420c188eae7a
parkmaeil <bitcocom@empas.com>
2026-09-12 22:29:49 +0900

    Update AiConfig.java

--- chapter03-2/src/main/java/com/spring/ai/chapter03_2/config/AiConfig.java
+++ chapter03-2/src/main/java/com/spring/ai/chapter03_2/config/AiConfig.java
@@ -4,8 +4,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
+    // @Bean(name="openAiChatClient") 생략가능
+    @Bean
+    @Primary
+    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.builder(openAiChatModel).build();
+    }
 
-
-
-
+    // @Bean(name="ollamaChatClient") 생략가능
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        return ChatClient.builder(ollamaChatModel).build();
+    }
 }
