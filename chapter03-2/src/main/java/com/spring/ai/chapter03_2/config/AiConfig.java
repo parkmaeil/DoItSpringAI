b186ad04f22fa0ebe0f4119d048cfa7aa045c4cf
parkmaeil <bitcocom@empas.com>
2026-09-12 22:30:52 +0900

    Clean up AiConfig by removing commented beans
    Removed commented-out bean definitions for ChatClient.

--- chapter03-2/src/main/java/com/spring/ai/chapter03_2/config/AiConfig.java
+++ chapter03-2/src/main/java/com/spring/ai/chapter03_2/config/AiConfig.java
@@ -4,16 +4,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
-    // @Bean(name="openAiChatClient") 생략가능
-    @Bean
-    @Primary
-    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
-        return ChatClient.builder(openAiChatModel).build();
-    }
 
-    // @Bean(name="ollamaChatClient") 생략가능
-    @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
-        return ChatClient.builder(ollamaChatModel).build();
-    }
+
+
+    
 }
